ae44d254b3458d99d8acc94980d976eb8b70f769
Janan Akkad <jananakkad@hotmail.co.uk>
2023-03-11 18:03:40 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    let thankYouCards = []
+    for (let i = 0; i < array.length; i++) {
+        thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--)
+    }
+}
